057a6428dad531ec023efcfc16ddbbacc9e19443
Mumin Quadri <mumin.quadri25@gmail.com>
2020-01-07 10:58:03 +0530

    Initial Commit

---
+++ CourseLibrary/CourseLibrary.API/Entities/Author.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CourseLibrary.API.Entities
+{
+    //this is the principal entity
+    public class Author
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string LastName { get; set; }
+
+        [Required]
+        public DateTimeOffset DateOfBirth { get; set; }          
+
+        [Required]
+        [MaxLength(50)]
+        public string MainCategory { get; set; }
+
+        public ICollection<Course> Courses { get; set; }
+            = new List<Course>();
+    }
+}
